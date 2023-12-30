c0b3a2add6770d8b65df070c99e5198e6b2ca716
Lirae Que Data <liraedata59@gmail.com>
2023-12-31 00:16:57 +0800

    Add espwifi class

---
+++ lib/ESPWifi/ESPWifi.cpp
@@ -0,0 +1,1 @@
+#include "ESPWifi.hpp"
---
+++ lib/ESPWifi/ESPWifi.hpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <Update.h>
+#include <LittleFS.h>
+#include <SD.h>
+#ifndef ESPWIFI_HPP
+#define ESPWIFI_HPP
+
+class ESPWifi
+{
+private:
+    String _ssid;
+    String _password;
+    int _counter;
+
+public:
+    ESPWifi();
+    bool IsConnect();
+    bool IsSuccess();
+};
+
+#endif
