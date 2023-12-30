b2ddfe0bbee89768db1cfc9642f1e3d12fd957e5
Lirae Que Data <liraedata59@gmail.com>
2023-12-31 01:56:38 +0800

    Update main for ESPBlue

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include <Arduino.h>
 #include <ESPWifi.hpp>
 
-String ssid = "Testing";
-String password = "12345678";
+// Bluetooth Setup
+// #include <ESPBlue.hpp>
+// const String SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
+// const String CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
+// ESPBlue bluetooth(SERVICE_UUID, CHARACTERISTIC_UUID);
+
+const String WIFI_SSID = "Testing";
+const String WIFI_PASSWORD = "12345678";
+
 
 void setup(){
   Serial.begin(115200);
 
-  ESPWifi wifi(ssid, password);
+  // Bluetooth Setup
+  // bluetooth.setup();
+
+  ESPWifi wifi(WIFI_SSID, WIFI_PASSWORD);
 
   Serial.print("Connecting to Wifi");
   while (!wifi.IsConnect()) {
@@ -20,8 +30,10 @@ void setup(){
   }
 
   if (wifi.IsConnect()){
-    Serial.println("Successfully connect to " + ssid);
+    Serial.println("Successfully connect to " + WIFI_SSID);
   }
+
+  
 }
 
 void loop(){
