72cb39abe957cd0bc0ca04aa695fbc3efa010878
Lirae Que Data <liraedata59@gmail.com>
2023-12-31 00:23:31 +0800

    Add ESPWifi implementation to src

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
+#include <ESPWifi.hpp>
 
-// put function declarations here:
-int myFunction(int, int);
+String ssid = "Testing";
+String password = "12345678";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup(){
+  Serial.begin(115200);
+
+  ESPWifi wifi(ssid, password);
+
+  Serial.print("Connecting to Wifi");
+  while (!wifi.IsConnect()) {
+
+    Serial.print(".");
+    if (!wifi.IsSuccess()) {
+      Serial.println("Error! Failed to Connect Wifi.");
+      break;
+    }
+  }
+
+  if (wifi.IsConnect()){
+    Serial.println("Successfully connect to " + ssid);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop(){
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
