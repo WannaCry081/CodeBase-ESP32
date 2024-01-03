efd14c35343c0fa5ac12b674fd28a59bbaa7cba5
Lirae Que Data <liraedata59@gmail.com>
2024-01-04 01:40:02 +0800

    Add DisplayStatus method

--- lib/ESPWifi/ESPWifi.cpp
+++ lib/ESPWifi/ESPWifi.cpp
@@ -2,15 +2,20 @@
 
 ESPWifi::ESPWifi(const String &ssid, const String &password) : _ssid(ssid), _password(password)
 {   
-    WiFi.begin(_ssid, _password);
+    WiFi.begin(ssid, password);
 }
 
-bool ESPWifi::IsConnect()
+bool ESPWifi::IsConnect()   
 {
-    return WiFi.status() != WL_CONNECTED;
+    return WiFi.status() == WL_CONNECTED;
 }
 
 bool ESPWifi::IsSuccess()
 {
     return _counter++ < 20;
+}
+
+void ESPWifi::DisplayStatus(){
+    Serial.println(WiFi.localIP());
+    Serial.println(WiFi.localIPv6());
 }
