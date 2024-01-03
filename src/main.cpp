d2e0e166883cbecb1a306e44807b351750fc2e1d
Lirae Que Data <liraedata59@gmail.com>
2024-01-04 01:41:06 +0800

    Update wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,33 @@
 #include <Arduino.h>
 #include <ESPWifi.hpp>
 
-// Bluetooth Setup
-// #include <ESPBlue.hpp>
-// const String SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
-// const String CHARACTERISTIC_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
-// ESPBlue bluetooth(SERVICE_UUID, CHARACTERISTIC_UUID);
+const String WIFI_SSID = "Pagstudyna";
+const String WIFI_PASSWORD = "Data2Santi";
 
-const String WIFI_SSID = "Testing";
-const String WIFI_PASSWORD = "12345678";
-
-
-void setup(){
+void setup()
+{
   Serial.begin(115200);
-
-  // Bluetooth Setup
-  // bluetooth.setup();
 
   ESPWifi wifi(WIFI_SSID, WIFI_PASSWORD);
 
-  Serial.print("Connecting to Wifi");
+  Serial.printf("Connecting to %s...", WIFI_SSID);
   while (!wifi.IsConnect()) {
-
     Serial.print(".");
-    if (!wifi.IsSuccess()) {
-      Serial.println("Error! Failed to Connect Wifi.");
-      break;
+    delay(1000);
+
+    if (!wifi.IsSuccess()){
+      Serial.printf("\nError! Failed to Connect to%s.\n",  WIFI_SSID);
     }
   }
 
   if (wifi.IsConnect()){
-    Serial.println("Successfully connect to " + WIFI_SSID);
+    Serial.printf("\nSuccessfully Connected to %s\n", WIFI_SSID);
+    wifi.DisplayStatus();
   }
 
-  
 }
 
-void loop(){
+void loop()
+{
 
 }
