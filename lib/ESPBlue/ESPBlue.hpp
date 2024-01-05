14f60ccd3a32813d435dd8ff87b6b25574f443e9
Lirae Que Data <liraedata59@gmail.com>
2024-01-06 03:13:07 +0800

    Add ESPBlue documentation

--- lib/ESPBlue/ESPBlue.hpp
+++ lib/ESPBlue/ESPBlue.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file ESPBlue.hpp
+ * @brief ESPBlue class definition for managing Bluetooth functionality on ESP32.
+ */
+
 #ifndef ESPBLUE_HPP
 #define ESPBLUE_HPP
 
@@ -6,6 +11,50 @@
 #include <BLEService.h>
 #include <BLEUtils.h>
 
+/**
+ * @class SampleCallback
+ * @brief A sample callback class for handling Bluetooth characteristic events.
+ */
+class SampleCallback : public BLECharacteristicCallbacks
+{
+public:
+    /**
+     * @brief Callback triggered when a central device writes to the characteristic.
+     * @param pCharacteristic The BLE characteristic being written.
+     */
+    void onWrite(BLECharacteristic *pCharacteristic);
+
+    /**
+     * @brief Callback triggered when a central device reads the value of the characteristic.
+     * @param pCharacteristic The BLE characteristic being read.
+     */
+    void onRead(BLECharacteristic *pCharacteristic);
+
+    /**
+     * @brief Callback triggered when a notification is sent from the characteristic.
+     * @param pCharacteristic The BLE characteristic sending the notification.
+     */
+    void onNotify(BLECharacteristic *pCharacteristic);
+
+    /**
+     * @brief Callback triggered when there is a status change in the characteristic.
+     * @param pCharacteristic The BLE characteristic with the status change.
+     * @param s The status type.
+     * @param code The status code.
+     */
+    void onStatus(BLECharacteristic *pCharacteristic, Status s, uint32_t code);
+
+    /**
+     * @brief Callback triggered when an indication is sent from the characteristic.
+     * @param pCharacteristic The BLE characteristic sending the indication.
+     */
+    void onIndicate(BLECharacteristic *pCharacteristic);
+};
+
+/**
+ * @class ESPBlue
+ * @brief ESPBlue class for managing Bluetooth functionality on ESP32.
+ */
 class ESPBlue
 {
 private:
@@ -13,15 +62,24 @@ private:
     String _serviceUUID;
     String _characteristicUUID;
 
-    class MyCallbacks : public BLECharacteristicCallbacks
-    {
-        void onWrite(BLECharacteristic *pCharacteristic);
-    };
-
 public:
+    /**
+     * @brief Constructor for initializing ESPBlue with service and characteristic UUIDs.
+     * @param serviceUUID The UUID of the Bluetooth service.
+     * @param characteristicUUID The UUID of the Bluetooth characteristic.
+     */
     ESPBlue(const String &serviceUUID, const String &characteristicUUID);
-    void setup();
-    void loop();
+
+    /**
+     * @brief Sets up the Bluetooth functionality.
+     */
+    void Setup(void);
+
+    /**
+     * @brief Sets the callbacks for the BLE characteristic.
+     * @param callbacks The BLE characteristic callbacks.
+     */
+    void SetCallbacks(BLECharacteristicCallbacks *callbacks);
 };
 
 #endif
