0682d21382672d88bb947ff0b364be9d9fb3e1f8
Lirae Que Data <liraedata59@gmail.com>
2024-01-06 00:37:50 +0800

    Add ESPWifi documentation

--- lib/ESPWifi/ESPWifi.hpp
+++ lib/ESPWifi/ESPWifi.hpp
@@ -2,23 +2,43 @@
 #define ESPWIFI_HPP
 
 #include <Arduino.h>
+#include <SD.h>
 #include <WiFi.h>
 #include <Update.h>
 #include <LittleFS.h>
-#include <SD.h>
 
 class ESPWifi
 {
 private:
-    String _ssid;
-    String _password;
-    int _counter;
+    short int _counter;     // Connection attempt counter
 
 public:
+    /**
+     * @brief Constructor for ESPWifi class.
+     * 
+     * @param ssid The WiFi SSID to connect to.
+     * @param password The password for the WiFi network.
+     */
     ESPWifi(const String &ssid, const String &password);
-    bool IsConnect();
-    bool IsSuccess();
-    void DisplayStatus();
+
+    /**
+     * @brief Checks if the ESP is connected to the WiFi network.
+     * 
+     * @return True if connected, false otherwise.
+     */
+    bool IsConnected(void);
+
+    /**
+     * @brief Checks if the last WiFi connection attempt was successful.
+     * 
+     * @return True if successful, false otherwise.
+     */
+    bool IsSuccess(void);
+
+    /**
+     * @brief Displays the current WiFi connection status.
+     */
+    void DisplayStatus(void);
 };
 
 #endif
